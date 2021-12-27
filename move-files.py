0b6edbc192eab2328f142776042468742969b0d8
SamarjeetBanik <samarjeetbanik@gmail.com>
2021-12-27 11:37:47 +0530

    added descriptive comments and refs

--- move-files.py
+++ move-files.py
@@ -1,32 +1,41 @@
+# `pip install watchdog` for below mentioned package to work!
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+# https://pypi.org/project/watchdog/ <- visit here to know more about "watchdog".
 
+# in-built standard utility modules
 import os
-import time
+import time 
 
+# event handler class to fetch the files from the mentioned paths
 class move_file_Handler(FileSystemEventHandler):
-    def on_modified(self, event):
+    def on_modified(self, event): # Called when a file or directory is modified.
         for each_file in os.listdir(main_folder):
             src = main_folder + "/" + each_file
             destination = destination_folder + "/" + each_file
             os.rename(src, destination)
 
+# paths (from "main_folder" to "destination_folder")
 main_folder = "/Users/ASUS/Downloads"
 destination_folder = "/Users/ASUS/Documents/New folder"
 
+# object instantiation
 event_handler = move_file_Handler()
 observer = Observer()
 
-observer.schedule(event_handler, main_folder, recursive=True)
-observer.start()
+observer.schedule(event_handler, main_folder, recursive=True) # 
+observer.start() # folder activity lookup/monitoring starts
 
 try:
     while True:
-        localtime = time.localtime()
-        result = time.strftime("%I:%M:%S %p", localtime)
-        print(result)
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
+        # debugging
+        # time_stamp = time.localtime()
+        # parsed_time_stamp = time.strftime("%I:%M:%S %p", time_stamp)
+        # print(parsed_time_stamp)
 
-observer.join()
+        # pause time of 1 sec
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop() # on interruption from keyboard (ctrl + c), the the program execution stops
+
+observer.join() # Wait until the thread terminates.
